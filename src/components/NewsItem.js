7522694384c04d42de48c2ca0e7c5ccdb106d1cd
Dev <dparmar1@isi-montreal.com>
2022-06-12 18:47:57 -0400

    Bugs fixed

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,7 +5,7 @@ function NewsItem(props) {
     return (
         <div className="card my-5" style={{ height: '35rem', border: '1px solid #9b4196' }}>
             <span className="position-absolute top-0 start-50 translate-middle badge bg-warning" style={{ border: '1px solid #9b4196' }}>{props.source}</span>
-            <img src={!props.imgURL ? 'https://i.ibb.co/d5fKBwb/default.png' : props.imgURL} className="card-img-top p-2" alt="..." style={{ height: '15rem', borderBottom: '1px solid #9b4196' }} />
+            <img src={!props.imgURL ? 'https://i.ibb.co/d5fKBwb/default.png' : props.imgURL} className="card-img-top p-1" alt="..." style={{ height: '15rem', borderBottom: '1px solid #9b4196' }} />
             <div className="card-body" style={{ height: '8rem' }}>
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text" >{props.desc}</p>
